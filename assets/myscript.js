6322b042337d4ed9dbc0d6def037b056e1c62b1a
QuangS2 <2051052108Quang@ou.edu.vn>
2021-08-02 22:13:13 +0700

    chuyen tiep slide

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,3 +1,4 @@
+var iSlide = 1;
 function standingTop()
 {
     var y = window.scrollY;
@@ -12,3 +13,30 @@ function standingTop()
         elm.style.position="static";
     }
 }
+function slideChange(i)
+{
+    document.getElementById("stick"+iSlide).style.color ="rgb(243,243,243,0.6)";
+    iSlide+=i;
+    if(iSlide==6) iSlide=1;
+    if(iSlide==0) iSlide=5;
+    slideNumber(iSlide);
+}
+function slideNumber(i)
+{
+    document.getElementById("stick"+iSlide).style.color ="rgb(243,243,243,0.6)";
+    iSlide = i;
+    document.getElementById("i-img").style.backgroundImage="url(/assets/images/slide"+iSlide+".jpg)";
+    document.getElementById("stick"+i).style.color ="rgb(54,54,54,0.6)";
+}
+// function loopSlide()
+// {
+//     var elm = document.getElementById("i-img");
+//     while (true)
+//     {
+//         elm.style.backgroundImage="url(/assets/images/slide"+iSlide+".jpg)";
+//         iSlide++;
+//         if(iSlide==6) iSlide =1;
+//         setTimeout(1000);
+//     }
+// }
+// loopSlide();
